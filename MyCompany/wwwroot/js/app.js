3106274eda02fc2665f0aca56aa5bc6f3006189b
TanyaMukha <t.almukhametova@gmail.com>
2024-12-04 00:13:29 +0200

    Добавьте файлы проекта.

---
+++ MyCompany/wwwroot/js/app.js
@@ -0,0 +1,13 @@
+﻿window.showLoading = function () {
+    const loadingElement = document.getElementById('loading');
+    if (loadingElement) {
+        loadingElement.style.display = 'block';
+    }
+}
+
+window.hideLoading = function () {
+    const loadingElement = document.getElementById('loading');
+    if (loadingElement) {
+        loadingElement.style.display = 'none';
+    }
+}
