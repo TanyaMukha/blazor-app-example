aa3f1cc22513ca2388e9216056113188db421945
TanyaMukha <t.almukhametova@gmail.com>
2024-12-04 11:28:24 +0200

    create constants & add images

---
+++ MyCompany/Data/Constants.cs
@@ -0,0 +1,21 @@
+﻿namespace MyCompany.Common
+{
+    public static class Constants
+    {
+        public static class Site
+        {
+            public const string Title = "Success IT company";
+            public const string Description = "We offer a full cycle of website development in accordance with your wishes";
+            public const string CompanyName = "Sunny Bunnies";
+            public const string Email = "sunny-bunnies@gmail.com";
+            public const string Phone = "+38 050 123 45 67";
+            public const string Location = "Slavutych, Ukraine";
+        }
+
+        public static class Colors
+        {
+            public const string Primary = "#18936F";
+            public const string Secondary = "#F9A826";
+        }
+    }
+}
